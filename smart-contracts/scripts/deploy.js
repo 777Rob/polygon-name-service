227e6cb89f3c939e7bab8bf37f7a6ca52a26c04a
Robertas <rcekanauskasnl@gmail.com>
2022-08-13 00:18:04 +0300

    add name registration to the frontend

--- smart-contracts/scripts/deploy.js
+++ smart-contracts/scripts/deploy.js
@@ -1,28 +1,58 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const [owner, superCoder] = await hre.ethers.getSigners();
+  const domainContractFactory = await hre.ethers.getContractFactory("Domains");
+  const domainContract = await domainContractFactory.deploy("ninja");
+  await domainContract.deployed();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  console.log("Contract owner:", owner.address);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // Let's be extra generous with our payment (we're paying more than required)
+  let txn = await domainContract.register("a16z", {
+    value: hre.ethers.utils.parseEther("1234"),
+  });
+  await txn.wait();
 
-  await lock.deployed();
+  // How much money is in here?
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  // Quick! Grab the funds from the contract! (as superCoder)
+  try {
+    txn = await domainContract.connect(superCoder).withdraw();
+    await txn.wait();
+  } catch (error) {
+    console.log("Could not rob contract");
+  }
+
+  // Let's look in their wallet so we can compare later
+  let ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+  console.log(
+    "Balance of owner before withdrawal:",
+    hre.ethers.utils.formatEther(ownerBalance)
+  );
+
+  // Oops, looks like the owner is saving their money!
+  txn = await domainContract.connect(owner).withdraw();
+  await txn.wait();
+
+  // Fetch balance of contract & owner
+  const contractBalance = await hre.ethers.provider.getBalance(
+    domainContract.address
+  );
+  ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+
+  console.log(
+    "Contract balance after withdrawal:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+  console.log(
+    "Balance of owner after withdrawal:",
+    hre.ethers.utils.formatEther(ownerBalance)
+  );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
