b0000c411aeaffdc9f961e1a91faaa3cda051b5c
Robertas <rcekanauskasnl@gmail.com>
2022-08-13 04:39:38 +0300

    refactor

--- smart-contracts/contracts/Domains.sol
+++ smart-contracts/contracts/Domains.sol
@@ -1,30 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
-
 pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import {StringUtils} from "./libraries/StringUtils.sol";
 import {Base64} from "./libraries/Base64.sol";
-import "hardhat/console.sol";
 
 contract Domains is ERC721URIStorage {
+    using Counters for Counters.Counter;
     string public _tld;
-    // We'll be storing our NFT images on chain as SVGs
     string svgPartOne =
         '<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none"><path fill="url(#B)" d="M0 0h270v270H0z"/><defs><filter id="A" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270"><feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/></filter></defs><path d="M72.863 42.949c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-10.081 6.032-6.85 3.934-10.081 6.032c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-8.013-4.721a4.52 4.52 0 0 1-1.589-1.616c-.384-.665-.594-1.418-.608-2.187v-9.31c-.013-.775.185-1.538.572-2.208a4.25 4.25 0 0 1 1.625-1.595l7.884-4.59c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v6.032l6.85-4.065v-6.032c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595L41.456 24.59c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-14.864 8.655a4.25 4.25 0 0 0-1.625 1.595c-.387.67-.585 1.434-.572 2.208v17.441c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l10.081-5.901 6.85-4.065 10.081-5.901c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v9.311c.013.775-.185 1.538-.572 2.208a4.25 4.25 0 0 1-1.625 1.595l-7.884 4.721c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-7.884-4.59a4.52 4.52 0 0 1-1.589-1.616c-.385-.665-.594-1.418-.608-2.187v-6.032l-6.85 4.065v6.032c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l14.864-8.655c.657-.394 1.204-.95 1.589-1.616s.594-1.418.609-2.187V55.538c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595l-14.993-8.786z" fill="#fff"/><defs><linearGradient id="B" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse"><stop stop-color="#cb5eee"/><stop offset="1" stop-color="#0cd7e4" stop-opacity=".99"/></linearGradient></defs><text x="32.5" y="231" font-size="27" fill="#fff" filter="url(#A)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif" font-weight="bold">';
     string svgPartTwo = "</text></svg>";
     uint256 constant priceLenThree = 0.05 ether;
     uint256 constant priceLenFour = 0.03 ether;
     uint256 constant priceDefault = 0.01 ether;
+    address payable public owner;
+
     mapping(string => address) public domains;
     mapping(string => string) public records;
-
-    // Add this at the top of your contract next to the other mappings
     mapping(uint256 => string) public names;
 
-    using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    address payable public owner;
 
     constructor(string memory __tld)
         payable
@@ -32,31 +28,35 @@ contract Domains is ERC721URIStorage {
     {
         _tld = __tld;
         owner = payable(msg.sender);
-
-        console.log("%s name service deployed", __tld);
     }
-
-    error Unauthorized();
-    error AlreadyRegistered();
-    error InvalidName(string name);
 
     modifier onlyOwner() {
         require(isOwner());
         _;
     }
 
-    // Add this anywhere in your contract body
+    error WithdrawalFailed();
+    error NameTaken();
+    error Unauthorized();
+    error AlreadyRegistered();
+    error InvalidName(string name);
+    error NotEnoughMatic();
+
+    /**
+     *  @dev Get all Registred names
+     */
     function getAllNames() public view returns (string[] memory) {
-        console.log("Getting all names from contract");
         string[] memory allNames = new string[](_tokenIds.current());
         for (uint256 i = 0; i < _tokenIds.current(); i++) {
             allNames[i] = names[i];
-            console.log("Name for token %d is %s", i, allNames[i]);
         }
 
         return allNames;
     }
 
+    /**
+     * @dev Check if a name is valid
+     */
     function valid(string calldata name) public pure returns (bool) {
         return StringUtils.strlen(name) >= 3 && StringUtils.strlen(name) <= 10;
     }
@@ -65,9 +65,11 @@ contract Domains is ERC721URIStorage {
         return msg.sender == owner;
     }
 
+    /**
+     * @dev With draw funds from the contract
+     */
     function withdraw() public onlyOwner {
         uint256 amount = address(this).balance;
-
         (bool success, ) = msg.sender.call{value: amount}("");
         require(success, "Failed to withdraw Matic");
     }
@@ -77,29 +79,18 @@ contract Domains is ERC721URIStorage {
      * @param name The domain name to register.
      */
     function register(string calldata name) public payable {
-        require(domains[name] == address(0));
         if (domains[name] != address(0)) revert AlreadyRegistered();
         if (!valid(name)) revert InvalidName(name);
         uint256 _price = price(name);
-        require(msg.value >= _price, "Not enough Matic paid");
+        if (msg.value < _price) revert NotEnoughMatic();
 
-        // Combine the name passed into the function  with the _TLD
         string memory _name = string(abi.encodePacked(name, ".", _tld));
-        // Create the SVG (image) for the NFT with the name
         string memory finalSvg = string(
             abi.encodePacked(svgPartOne, _name, svgPartTwo)
         );
         uint256 newRecordId = _tokenIds.current();
         uint256 length = StringUtils.strlen(name);
         string memory strLen = Strings.toString(length);
-
-        console.log(
-            "Registering %s.%s on the contract with tokenID %d",
-            name,
-            _tld,
-            newRecordId
-        );
-
         // Create the JSON metadata of our NFT. We do this by combining strings and encoding as base64
         string memory json = Base64.encode(
             abi.encodePacked(
@@ -117,14 +108,6 @@ contract Domains is ERC721URIStorage {
             abi.encodePacked("data:application/json;base64,", json)
         );
 
-        console.log(
-            "\n--------------------------------------------------------"
-        );
-        console.log("Final tokenURI", finalTokenUri);
-        console.log(
-            "--------------------------------------------------------\n"
-        );
-
         _safeMint(msg.sender, newRecordId);
         _setTokenURI(newRecordId, finalTokenUri);
         domains[name] = msg.sender;
@@ -139,7 +122,7 @@ contract Domains is ERC721URIStorage {
      */
     function price(string calldata name) public pure returns (uint256) {
         uint256 len = StringUtils.strlen(name);
-        require(len > 0);
+        if (len < 0) revert InvalidName(name);
         if (len == 3) {
             return priceLenThree;
         } else if (len == 4) {
@@ -149,18 +132,31 @@ contract Domains is ERC721URIStorage {
         }
     }
 
+    /**
+     * @dev Get the owner of a domain name.
+     * @param name The domain name to get the owner of.
+     * @return owner of the domain name.
+     */
     function getAddress(string calldata name) public view returns (address) {
         return domains[name];
     }
 
+    /**
+     * @dev Set record for a domain name.
+     * @param name The domain name to get the owner of.
+     * @param record The record to set.
+     */
     function setRecord(string calldata name, string calldata record) public {
         // Check that the owner is the transaction sender
         if (msg.sender != domains[name]) revert Unauthorized();
-
-        require(domains[name] == msg.sender);
         records[name] = record;
     }
 
+    /**
+     * @dev Get record for a domain name.
+     * @param name The domain name to get the record of.
+     * @return record of the domain name.
+     */
     function getRecord(string calldata name)
         public
         view
