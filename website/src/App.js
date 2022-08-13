1bde035623e3338fe746d5d4ea9464efa5d23623
Robertas <rcekanauskasnl@gmail.com>
2022-08-13 04:25:04 +0300

    refactor

--- website/src/App.js
+++ website/src/App.js
@@ -1,25 +1,219 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { DomainsContract } from "./contracts/DomainsContract";
+import "./styles/App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
+import { ethers } from "ethers";
+import polygonLogo from "./assets/polygonlogo.png";
+import ethLogo from "./assets/ethlogo.png";
+import { connect } from "./state";
+import ConnectWallet from "./components/ConnectWallet";
+import Mints from "./components/Mints";
 
-function App() {
+const TWITTER_HANDLE = "_buildspace";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const tld = ".matic";
+
+const App = (props) => {
+  const { currentAccount, switchNetwork, network, mints, fetchMints } = props;
+  const [domain, setDomain] = useState("");
+  const [record, setRecord] = useState("");
+  const [editing, setEditing] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const updateDomain = async () => {
+    if (!record || !domain) {
+      return;
+    }
+    setLoading(true);
+    console.log("Updating domain", domain, "with record", record);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          DomainsContract.address,
+          DomainsContract.abi,
+          signer
+        );
+
+        let tx = await contract.setRecord(domain, record);
+        await tx.wait();
+        console.log("Record set https://mumbai.polygonscan.com/tx/" + tx.hash);
+
+        fetchMints();
+        setRecord("");
+        setDomain("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  const inputForm = () => {
+    if (network !== "Polygon Mumbai Testnet") {
+      return (
+        <div className="connect-wallet-container">
+          <p>Please connect to Polygon Mumbai Testnet</p>
+          <button className="cta-button mint-button" onClick={switchNetwork}>
+            Click here to switch
+          </button>
+        </div>
+      );
+    }
+
+    return (
+      <div className="form-container">
+        <div className="first-row">
+          <input
+            type="text"
+            value={domain}
+            placeholder="Domain Name"
+            disabled={editing}
+            onChange={(e) => setDomain(e.target.value)}
+          />
+          <p className="tld"> {tld} </p>
+        </div>
+
+        <input
+          type="text"
+          value={record}
+          placeholder="Domain Record"
+          onChange={(e) => setRecord(e.target.value)}
+        />
+        {/* If the editing variable is true, return the "Set record" and "Cancel" button */}
+        {editing ? (
+          <div className="button-container">
+            <button
+              className="cta-button mint-button"
+              disabled={loading}
+              onClick={updateDomain}
+            >
+              Set record
+            </button>
+            <button
+              className="cta-button mint-button"
+              onClick={() => {
+                setEditing(false);
+              }}
+            >
+              Cancel
+            </button>
+          </div>
+        ) : (
+          // If editing is not true, the mint button will be returned instead
+          <button
+            className="cta-button mint-button"
+            disabled={loading}
+            onClick={mintDomain}
+          >
+            Register
+          </button>
+        )}
+      </div>
+    );
+  };
+
+  const mintDomain = async () => {
+    if (!domain) {
+      return;
+    }
+    if (domain.length < 3) {
+      alert("Domain must be at least 3 characters long");
+      return;
+    }
+    // 3 chars = 0.05 MATIC, 4 chars = 0.03 MATIC, 5 or more = 0.01 MATIC
+    const price =
+      domain.length === 3 ? "0.05" : domain.length === 4 ? "0.03" : "0.01";
+
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          DomainsContract.address,
+          DomainsContract.abi,
+          signer
+        );
+
+        let tx = await contract.register(domain, {
+          value: ethers.utils.parseEther(price),
+        });
+        const receipt = await tx.wait();
+
+        if (receipt.status === 1) {
+          console.log(
+            "Domain minted! https://mumbai.polygonscan.com/tx/" + tx.hash
+          );
+
+          // Set the record for the domain
+          tx = await contract.setRecord(domain, record);
+          await tx.wait();
+
+          console.log(
+            "Record set! https://mumbai.polygonscan.com/tx/" + tx.hash
+          );
+
+          // Call fetchMints after 2 seconds
+          setTimeout(() => {
+            fetchMints();
+          }, 2000);
+
+          setRecord("");
+          setDomain("");
+        } else {
+          alert("Transaction failed! Please try again");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // This will take us into edit mode and show us the edit buttons!
+  const editRecord = (name) => {
+    setEditing(true);
+    setDomain(name);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="header-container">
+          <header>
+            <div className="left">
+              <p className="title">
+                <img
+                  style={{ width: "30px", height: "30px" }}
+                  src={polygonLogo}
+                />
+                Polygon Name Service
+              </p>
+              <p className="subtitle">
+                Your immortal domain on the blockchain!
+              </p>
+            </div>
+          </header>
+        </div>
+
+        {!currentAccount && <ConnectWallet />}
+        {currentAccount && inputForm()}
+        {mints && <Mints editRecord={editRecord} />}
+
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built with @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default connect(App);
